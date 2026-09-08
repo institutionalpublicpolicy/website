40de7c069b875ff459b040e9a6f69a7dbe051e85
institutionalpublicpolicy <editorial@institutionalpublicpolicy.org>
2026-09-08 12:09:40 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -9,7 +9,8 @@ const rss = require("@11ty/eleventy-plugin-rss");
 const LANGS = ["en", "fr", "it"];
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(rss);eleventyConfig.addGlobalData("buildStamp", () => Date.now().toString(36));
+  eleventyConfig.addPlugin(rss);
+eleventyConfig.addGlobalData("buildStamp", () => Date.now().toString(36));
 
   // static assets copied through untouched
   eleventyConfig.addPassthroughCopy({ "src/style.css": "style.css" });
